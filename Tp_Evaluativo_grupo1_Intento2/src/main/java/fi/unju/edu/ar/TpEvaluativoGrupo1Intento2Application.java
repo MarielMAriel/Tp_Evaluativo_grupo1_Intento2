1e093392d4dcbc30185d4c21e2b9646943b25598
Pc <Pc@DESKTOP-BE8SGV4>
2022-05-30 01:47:06 -0300

    carga de  datos

---
+++ Tp_Evaluativo_grupo1_Intento2/src/main/java/fi/unju/edu/ar/TpEvaluativoGrupo1Intento2Application.java
@@ -0,0 +1,13 @@
+package fi.unju.edu.ar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpEvaluativoGrupo1Intento2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpEvaluativoGrupo1Intento2Application.class, args);
+	}
+
+}
